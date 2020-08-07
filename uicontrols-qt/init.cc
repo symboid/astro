01b05d338312c3efc2007544a2fc6514d91f062b
Török Róbert <robert.torok@gmail.com>
2020-08-07 21:05:34 +0200

    fix of astro version injection

--- uicontrols-qt/init.cc
+++ uicontrols-qt/init.cc
@@ -23,12 +23,8 @@ mod_astro_uicontrols_qt::mod_astro_uicontrols_qt()
     load_translator();
 
     arh::main_object<QSoftwareUpdate> softwareUpdate;
-    softwareUpdate->setAstroVersion(
-        COMPONENT_VER_MAJOR,
-        COMPONENT_VER_MINOR,
-        COMPONENT_VER_PATCH,
-        COMPONENT_VER_SERIAL,
-        COMPONENT_REV_ID);
+    softwareUpdate->addComponentVersion(COMPONENT_NAME, COMPONENT_VER_MAJOR, COMPONENT_VER_MINOR,
+                        COMPONENT_VER_PATCH, COMPONENT_VER_SERIAL, COMPONENT_REV_ID);
 }
 
 mod_astro_uicontrols_qt::~mod_astro_uicontrols_qt()
