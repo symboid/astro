d02593d3e7c13e7d09fddfd2a6327eee44e40cb2
symboid <torok.robert@symboid.com>
2020-08-07 09:02:25 +0200

    sdk version injection added

--- uicontrols-qt/init.cc
+++ uicontrols-qt/init.cc
@@ -8,6 +8,9 @@
 #include <QCoreApplication>
 #include <QLocale>
 
+#include "sdk/hosting/qsoftwareupdate.h"
+#include "astro/component.h"
+
 mod_astro_uicontrols_qt::mod_astro_uicontrols_qt()
 {
     qRegisterMetaType<QArcCoord::Degree>("Degree");
@@ -18,6 +21,14 @@ mod_astro_uicontrols_qt::mod_astro_uicontrols_qt()
 
     Q_INIT_RESOURCE(astro_uicontrols_qt);
     load_translator();
+
+    arh::main_object<QSoftwareUpdate> softwareUpdate;
+    softwareUpdate->setAstroVersion(
+        COMPONENT_VER_MAJOR,
+        COMPONENT_VER_MINOR,
+        COMPONENT_VER_PATCH,
+        COMPONENT_VER_SERIAL,
+        COMPONENT_REV_ID);
 }
 
 mod_astro_uicontrols_qt::~mod_astro_uicontrols_qt()
