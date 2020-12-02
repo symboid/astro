db9cdd599116730c0dfc8bd9129a7f020d58752b
Török Róbert <robert.torok@gmail.com>
2020-12-02 19:41:45 +0100

    template lib astro/calculo purged

--- calculo/init.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef __SYMBOID_ASTRO_CALCULO_INIT_H__
-#define __SYMBOID_ASTRO_CALCULO_INIT_H__
-
-#include "astro/calculo/defs.h"
-
-#endif // __SYMBOID_ASTRO_CALCULO_INIT_H__
--- calculo/setup.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef __SYMBOID_ASTRO_CALCULO_SETUP_H__
-#define __SYMBOID_ASTRO_CALCULO_SETUP_H__
-
-#include "astro/calculo/defs.h"
-
-#endif // __SYMBOID_ASTRO_CALCULO_SETUP_H__
