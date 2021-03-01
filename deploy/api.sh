9c837bf0fc14567c26a96169af4265f23d4f2ee2
Török Róbert <robert.torok@gmail.com>
2021-03-01 21:03:45 +0100

    Create api.sh

---
+++ deploy/api.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source $(dirname "$0")/../../build/deploy/unix/api.sh
+
+REL_OUTPUT_DIR=$1
+REL_ARCHIVE_DIR=$2
+
+ComponentApiBegin astro $REL_OUTPUT_DIR $REL_ARCHIVE_DIR
+FolderApi $COMPONENT_NAME/eph *.h
+ModuleApi db
+ModuleApi controls
+ModuleApi hora
+FolderApi $COMPONENT_NAME/db/sweph/ephe *.*
+ComponentApiEnd
+
